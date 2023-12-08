48b81d1edb4dd10f8c6633b8d5f89c24434ef479
addust <andikafahrezi5@gmail.com>
2023-12-08 12:58:08 +0700

    minor update

--- NgopSkuy/NgopSkuy/js/script.js
+++ NgopSkuy/NgopSkuy/js/script.js
@@ -41,30 +41,6 @@ document.addEventListener("click", function (e) {
   }
 });
 
-//Modal box
-const itemDetailModal = document.querySelector("#item-detail-modal");
-const itemDetailButtons = document.querySelectorAll(".item-detail-button");
-
-itemDetailButtons.forEach((btn) => {
-  btn.onclick = (e) => {
-    itemDetailModal.style.display = "flex";
-    e.preventDefault();
-  };
-});
-
-// tombol close shopping modal
-document.querySelector(".modal .close-icon").onclick = (e) => {
-  itemDetailModal.style.display = "none";
-  e.preventDefault();
-};
-
-//klik luar modal
-window.onclick = (e) => {
-  if (e.target === itemDetailModal) {
-    itemDetailModal.style.display = "none";
-  }
-};
-
 window.addEventListener("load", function () {
   const form = document.getElementById("formulir");
   form.addEventListener("submit", function (e) {
